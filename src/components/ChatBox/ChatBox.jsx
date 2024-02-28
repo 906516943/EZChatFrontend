d89c958c749fee95aab810285057c17546e094ce
Zheng <48804310+zh1322114772@users.noreply.github.com>
2024-02-27 20:22:15 -0500

    added tailwind css

---
+++ src/components/ChatBox/ChatBox.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+class ChatBox extends React.Component
+{ 
+    
+
+    render()
+    { 
+        return <h2 className="text-3xl font-bold underline">Hello world</h2>
+    }
+}
+
+
+
+
+export default ChatBox;
